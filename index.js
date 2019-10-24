cabdc86cee87a13c06c393297dd78683855cfe05
bmurdock <brian.j.murdock@gmail.com>
2019-10-24 09:08:01 -0600

    not sure

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const parser = require('body-parser');
-
+const port = process.env.PORT || 3232;
 const app = express();
 
 app.use(express.static('public'));
+
+
+app.post('/form', express.json(), parser({ extended: true }), (req, res) => {
+    /*
+    Do something here
+    */
+    console.log('Form data submitted.');
+    console.log('body: ', req.body);
+    console.log('params: ', req.params);
+    console.log('query: ', req.query);
+
+
+    res.status(200);
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        console.error('Error launching server! ', err);
+    }
+    console.log(`Server listening on port ${port}`);
+});
